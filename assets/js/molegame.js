fbaa38ef63de1432eb28f5e43e37af2a36ae29b2
Sonu kumar kushwaha <flyingsonu736@gmail.com>
2021-03-01 23:29:15 +0530

    made Whack a Mole Game responsive for all screen sizes (#201)
    * Update molegame.js
    
    * Update molegame.css
    
    * Update molegame.html

--- assets/js/molegame.js
+++ assets/js/molegame.js
@@ -1,50 +1,50 @@
 const holes = document.querySelectorAll('.hole');
-        const scoreBoard = document.querySelector('.score');
-        const moles = document.querySelectorAll('.mole');
-        let lastHole;
-        let timeUp = false;
-        let score = 0;
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
 
-        function randomTime(min, max) {
-            return Math.round(Math.random() * (max - min) + min);
-        }
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
 
-        function randomHole(holes) {
-            const idx = Math.floor(Math.random() * holes.length);
-            const hole = holes[idx];
-            if (hole === lastHole){
-                console.log('Ah nah thats the same one bud');
-                return randomHole(holes);
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        console.log('Ah nah thats the same one bud');
+        return randomHole(holes);
 
-            }
-            lastHole = hole;
-            return hole;
-        }
+    }
+    lastHole = hole;
+    return hole;
+}
 
-        function peep() {
-            const time = randomTime(200, 1000);
-            const hole = randomHole(holes);
-            hole.classList.add('up');
-            setTimeout(() => {
-                hole.classList.remove("up");
-                if (!timeUp) peep();
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if (!timeUp) peep();
 
-            },time);
-        }
+    }, time);
+}
 
-        function startGame() {
-            scoreBoard.textContent = 0;
-            timeUp =  false;
-            score = 0;
-            peep();
-            setTimeout(() => timeUp = true,10000)
-        }
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000)
+}
 
-        function bonk(e) {
-            if(!e.isTrusted) return; //cheater!
-            score++;
-            this.parentNode.classList.remove('up');
-            scoreBoard.textContent = score;
-        }
+function bonk(e) {
+    if (!e.isTrusted) return; //cheater!
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+}
 
-        moles.forEach(mole => mole.addEventListener('click',bonk));
+moles.forEach(mole => mole.addEventListener('click', bonk));
