4d46f2ff661c9f472d21e28ef3a821aeb24d3242
Aayush-hub <ayushgarg1951@gmail.com>
2021-03-09 12:52:03 +0530

    Added Typing Speed Tester

--- script.js
+++ script.js
@@ -155,10 +155,15 @@ let projectData = [
     projectUrl: "public/Tic_Tac_Toe.html",
   },
   {
-        projectName: "DragNDrop",
-        projectImage: "assets/GIFs/DragNDrop.gif",
-        projectUrl: "public/DragNDrop.html",
-   },
+    projectName: "DragNDrop",
+    projectImage: "assets/GIFs/DragNDrop.gif",
+    projectUrl: "public/DragNDrop.html",
+  },
+  {
+    projectName: "Typing Speed Tester",
+    projectImage: "assets/GIFs/typing_speed_test.png",
+    projectUrl: "public/typing_speed_test.html",
+  },
   {
     projectName: "Quiz App",
     projectImage: "assets/GIFs/quizApp.gif",
@@ -190,7 +195,7 @@ function getProjects() {
   let output = "";
   projectData.forEach(
     (data, item) =>
-      (output += `
+    (output += `
           <div class="card my-3 mx-auto col cardStyle">
             <a href=${data.projectUrl} class="text-decoration-none" target="_blank">
               <img
