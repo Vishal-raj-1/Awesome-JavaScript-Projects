7c2bae038bd573c6c6f1ff134dac6f107095392f
Gaurav Maheshwari <gaurav02maheshwari@gmail.com>
2021-03-10 23:22:01 +0530

    Added the project details
    Added the project details in blurry_loading_interface.js file.

--- assets/js/blurry_loading_interface.js
+++ assets/js/blurry_loading_interface.js
@@ -14,3 +14,9 @@ function blurring(){
     loadtext.style.opacity = `${1-load/100}`
     bg.style.filter = `blur(${30-(load/100)*30}px)`
 }
+// This project is based on the starter page of any website which is a Blurry effect with a countdown from 0% to 100%.
+// The structure is made from blurry_loading_interface.html file.
+// To butify The project we use the blurry_loading_interface.css file.
+// The main brain of the body that is how will the opacity change from 100% to 0% is written under this javascript file that is blurry_loading_interface.js file.
+// You can use this repository to add a starter page to your website and showcase it to your friends or for company basis as well.
+// Thank you guys for viewing this project... 
