8e6d8a911e87b60dabaed325a3d6350687c8b318
Satyam Dengre <59261812+satyampgt4@users.noreply.github.com>
2021-03-13 03:54:38 +0530

    Dynamic calendar (#271)
    * Dynamic Calender is added
    
    * text to voice project (#256)
    
    * text to voice project
    
    * textVoice added to script.js
    
    * corrected typo error in local storage display-card (#292)
    
    Co-authored-by: Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
    Co-authored-by: ishita-sehgal <78426468+ishita-sehgal@users.noreply.github.com>

--- script.js
+++ script.js
@@ -193,6 +193,16 @@ let projectData = [
     projectName: "Animated Minion Eyes",
     projectImage: "assets/GIFs/Minion-Eyes.gif",
     projectUrl: "public/Minion-Eyes.html",
+  },
+  {
+    projectName: "calendar",
+    projectImage: "assets/GIFs/cal.gif",
+    projectUrl: "public/calendar.html",
+  },
+  {
+    projectName: "Text to Voice Generator",
+    projectImage: "assets/GIFs/textToVoice",
+    projectUrl: "public/textToVoice.html",
   }
 ];
 
