8c11f9d5fc6a4d7f9a94827382b5fecec82f7e2e
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-13 09:33:09 +0530

    Update AND,OR,XOR Converter.js

--- assets/js/AND,OR,XOR Converter.js
+++ assets/js/AND,OR,XOR Converter.js
@@ -1,32 +1,27 @@
 
-        // console.log(number2);
+        //AND function
         function and(){
             var number1=document.getElementById("value1").value;
             var number2=document.getElementById("value2").value;
-            // alert(number1)
-        //     console.log(number1)
-        // console.log(number2)
             var ans=parseInt(number1) & parseInt(number2);
-            // console.log(ans)
+        
             document.getElementById("answer").value=ans;
         }
+        //OR function
         function or(){
             let number1=document.getElementById("value1").value;
             let number2=document.getElementById("value2").value;
-            // alert(number1)
-        //     console.log(number1)
-        // console.log(number2)
+            
             let ans=parseInt(number1) | parseInt(number2);
-            // console.log(ans)
+        
             document.getElementById("answer").value=ans;
         }
+        //XOR function
         function xor(){
             let number1=document.getElementById("value1").value;
             let number2=document.getElementById("value2").value;
-            // alert(number1)
-        //     console.log(number1)
-        // console.log(number2)
+           
             let ans=parseInt(number1) ^ parseInt(number2);
-            // console.log(ans)
+         
             document.getElementById("answer").value=ans;
         }
