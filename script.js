899a09f92bb364757e442bdf945e1b8110dd9b79
sidmohanty11 <sidmohanty11@gmail.com>
2021-03-13 11:39:52 +0530

    adding ayoga app with music for pranayam yoga

--- script.js
+++ script.js
@@ -203,6 +203,11 @@ let projectData = [
     projectName: "Text to Voice Generator",
     projectImage: "assets/GIFs/textToVoice",
     projectUrl: "public/textToVoice.html",
+  },
+  {
+    projectName: "Yoga App",
+    projectImage: "assets/Images/yogass.png",
+    projectUrl: "public/yoga.html",
   }
 ];
 
