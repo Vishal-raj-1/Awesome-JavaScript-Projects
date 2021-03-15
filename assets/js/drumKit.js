c5610ace93b1fb237b548394e6e2c27b443a6b27
aditidewangan <aditidewangan1012@gmail.com>
2021-03-15 21:19:57 +0530

    Modified Drum kit and change its theme

--- assets/js/drumKit.js
+++ assets/js/drumKit.js
@@ -1,24 +1,81 @@
-window.addEventListener("keydown", playAudio);
+var numberOfButtons = document.querySelectorAll(".drum").length;
 
-function playAudio(e) {
-  // To instantiate audio
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+for(var i = 0 ; i <= numberOfButtons ; i++){
 
-  if (!audio) return;
-  audio.play();
-  audio.currentTime = 0;
+  document.querySelectorAll("button")[i].addEventListener("click" , function (){
 
-  // To animate key clicked
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    var buttonInnerHTML = this.innerHTML;
 
-  key.classList.add("key-playing");
-}
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+    });
+
+  }
+
+    function makeSound(key){
+
+      switch(key) {
+
+        case "A":
+        var boom = new Audio("../assets/sounds/boom.wav");
+        boom.play();
+        break;
+
+        case "S":
+        var clap = new Audio("../assets/sounds/clap.wav");
+        clap.play();
+        break;
+
+        case "D":
+        var hihat = new Audio("../assets/sounds/hihat.wav");
+        hihat.play();
+        break;
+
+        case "F":
+        var boom = new Audio("../assets/sounds/boom.wav");
+        boom.play();
+        break;
+
+        case "G":
+        var openhat = new Audio("../assets/sounds/openhat.wav");
+        openhat.play();
+        break;
+
+        case "H":
+        var ride = new Audio("../assets/sounds/ride.wav");
+        ride.play();
+        break;
+
+        case "J":
+        var snare = new Audio("../assets/sounds/snare.wav");
+        snare.play();
+        break;
+
+        case "K":
+        var tink = new Audio("../assets/sounds/tink.wav");
+        tink.play();
+        break;
+
+        case "L":
+        var tom = new Audio("../assets/sounds/tom.wav");
+        tom.play();
+        break;
+
+        default : console.log(key);
+    }
+  }
+
 
 // To remove animation on key clicked
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("key-playing");
-}
+function buttonAnimation(currentkey){
 
-const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+var activeButton = document.querySelector("." + currentkey);
+activeButton.classList.add("pressed");
+
+setTimeout(function () {
+  activeButton.classList.remove("pressed");
+}, 100);
+
+}
