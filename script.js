498d853321e62c3572919826f12cc33075d09b20
mohammedjameel24 <mohammedjameel2412@gmail.com>
2021-03-28 10:02:01 +0530

    Added MemeUrl

--- script.js
+++ script.js
@@ -193,11 +193,16 @@ let projectData = [
     projectName: "Animated Minion Eyes",
     projectImage: "assets/GIFs/Minion-Eyes.gif",
     projectUrl: "public/Minion-Eyes.html",
-  }
+  },
   {
     projectName: "Text to Voice Generator",
     projectImage: "assets/GIFs/textToVoice",
     projectUrl: "public/textToVoice.html",
+  },
+  {
+    projectName: "MemeUrl",
+    projectImage: "assets/GIFs/memeUrl.png",
+    projectUrl: "memeUrl.html",
   }
 ];
 
