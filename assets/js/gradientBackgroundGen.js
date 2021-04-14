882d6f2032e4b42b63e59c0f9c6994a729859d3e
Nirbhay Parmar <niravnirbhay94@gmail.com>
2021-04-15 00:33:45 +0530

    commented the gradientBackgroundGen.js file

--- assets/js/gradientBackgroundGen.js
+++ assets/js/gradientBackgroundGen.js
@@ -1,9 +1,10 @@
-var color1 = document.getElementById("c1");
-var color2 = document.getElementById("c2");
+var color1 = document.getElementById("c1"); //to get the value of color 1
+var color2 = document.getElementById("c2"); // to get the value of color 2
 var css = document.getElementById("code");
 var body = document.getElementById("gradient");
 var button = document.getElementById("random");
 
+// function to generate random hexadecimal values
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
   var color = "#";
@@ -11,12 +12,14 @@ function getRandomColor() {
   return color;
 }
 
+// function to dynamically set css values for linear gradient
 function setgradient() {
   body.style.background =
     "linear-gradient(to right , " + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
 }
 
+// function to receive random hexadecimal values from getRandomColor()
 function randomgradient() {
   var randomColor1 = getRandomColor();
   var randomColor2 = getRandomColor();
@@ -27,6 +30,7 @@ function randomgradient() {
   css.textContent = body.style.background + ";";
 }
 
+// event listener to the input button and click random button and also for reload of the web app
 color1.addEventListener("input", setgradient);
 color2.addEventListener("input", setgradient);
 button.addEventListener("click", randomgradient);
