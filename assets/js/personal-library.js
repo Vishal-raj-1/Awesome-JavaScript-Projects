25cce5c207bbf10a1d6639a8076a7a0577011303
ireneelizabethsabu <irene@cet.ac.in>
2021-04-20 12:15:59 +0530

    methods to add a new book on clicking add btn

--- assets/js/personal-library.js
+++ assets/js/personal-library.js
@@ -12,3 +12,21 @@ class Book{
 let myLibrary = [
     {title:"Alice in WonderLand",author: "Enid Blyton", isRead: "Read"}
 ];
+//Adding a new Book
+const addBook = (e) => {
+    let title = document.getElementById("title");
+    let author = document.getElementById("author");
+    const isRead = document.getElementById("read-status").value;
+    if(title.value !== "" && author.value!== ""){
+        myLibrary.push(new Book(title.value,author.value,isRead));
+        //displayBooks();
+        title.value = "";
+        author.value = "";
+    }
+}
+//Add button eventListener
+const addBookBtn = document.getElementById("add");
+addBookBtn.onclick = (e) => {
+    e.preventDefault();
+    addBook();
+}
