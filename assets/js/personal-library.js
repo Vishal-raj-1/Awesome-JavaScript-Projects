0487639798a236394a72da281e2162dacd328402
ireneelizabethsabu <irene@cet.ac.in>
2021-04-20 12:13:54 +0530

    Adding Book Class and myLibrary array

---
+++ assets/js/personal-library.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Book Object
+class Book{
+    constructor(title="Unknown",author="Unknown",isRead){
+        this.title = title;
+        this.author = author;
+        this.isRead = isRead;
+    }
+}
+//Book Array
+let myLibrary = [
+    {title:"Alice in WonderLand",author: "Enid Blyton", isRead: "Read"}
+];
