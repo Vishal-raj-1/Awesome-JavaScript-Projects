6c6085c002ee92bc9c0627e7e2c8e84441e62e08
Aashi Thakkar <52272710+aashi2912@users.noreply.github.com>
2021-05-13 15:23:28 +0530

    Update PigGame.js

--- assets/js/PigGame.js
+++ assets/js/PigGame.js
@@ -51,7 +51,7 @@ btnRoll.addEventListener("click", function () {
 
     // 2. Display dice
     diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `../assets/Images/PigGame/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
