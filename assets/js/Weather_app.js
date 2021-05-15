1d6c5b15fb866dc5f9562810eefaeee46bdbe998
manisha-27 <67019423+manisha-27@users.noreply.github.com>
2021-05-15 21:34:39 +0530

    Update Weather_app.js

--- assets/js/Weather_app.js
+++ assets/js/Weather_app.js
@@ -1,170 +1,47 @@
-let loc =document.getElementById("location");
-let tempicon=document.getElementById("temp-icon");
-let tempvalue=document.getElementById("temp-value");
-let tempunit=document.getElementById("temp-unit");
-let temperatureSection = document.querySelector('.temperature-section');
-let climate =document.getElementById("climate");
-let iconfile;
-const searchInput=document.getElementById("search-input");
-const searchButton=document.getElementById("search-button");
-
-searchButton.addEventListener('click', (e)=>
-{
-
-e.preventDefault();
-getWeather(searchInput.value);
-searchInput.value='';
-
-
-});
-
-
-// for any particular city
-const getWeather=async (city)=>
-{
-    try{
-        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dab3af44de7d24ae7ff86549334e45bd`,
-   
-            {mode: 'cors'}
-        );
-
-        const weatherData= await response.json();
-        console.log(weatherData);
-        const{name}=weatherData;
-        const{feels_like}=weatherData.main;
-        const{id,main}=weatherData.weather[0];
-        loc.textContent=name;
-        climate.textContent=main;
-        tempvalue.textContent=(feels_like-273).toFixed(2);
-        if(id<300 && id>200)
-        {
-            tempicon.src="../assets/Images/weather_icons/thunderstorm.svg"
-        }
-       else  if(id<400 && id>300)
-        {
-            tempicon.src="../assets/Images/weather_icons/cloud-solid.svg"
-        }
-       else if(id<600&& id>500)
-        {
-            tempicon.src="../assets/Images/weather_icons/rain.svg"
-        }
-       else  if(id<700 && id>600)
-        {
-            tempicon.src="../assets/Images/weather_icons/snow.svg"
-        }
-       else  if(id<800 && id>700)
-        {
-            tempicon.src="../assets/Images/weather_icons/clouds.svg"
-        }
-         else if(id==800)
-        {
-            tempicon.src="../assets/Images/weather_icons/clouds-and-sun.svg"
-        }
-
-        //Change Temperature to Celsius/Fahrenheit
-        temperatureSection.addEventListener('click' , ()=>{
-            if(tempunit.textContent==="F"){
-                tempunit.textContent = "°C";
-                tempvalue.textContent=(feels_like-273).toFixed(2);
-                
-            }
-            else{
-                tempunit.textContent = "F";
-                tempvalue.textContent= 
-                ((feels_like- 273.15) * 9/5+ 32).toFixed(2);
-            }
-        });
-   
+let weather = {
+    apiKey: "78c169846cb53cc1a3e17a1c10e67a96",
+    fetchWeather: function(city){
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            +"&appid="
+            + this.apiKey
+        )
+          .then((response) => response.json())
+          .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const{name}=data;
+        const{icon,description}=data.weather[0];
+        const{temp,humidity}=data.main;
+        const{speed}=data.wind;
+        // console.log(name,icon,description,temp,humidity,speed);    //for testing in the console
+        document.querySelector(".city").innerText ="Weather in "+name;
+        document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+ icon +".png";
+        document.querySelector(".description").innerText= description;
+        document.querySelector(".temp").innerText =temp +"°C";
+        document.querySelector(".humidity").innerText ="Humidity"+ humidity +"%";
+        document.querySelector(".wind").innerText ="Wind Speed"+ speed +"km/hr";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + " landscape')"
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-    catch(error)
-    {
-        alert('City not found');
-    }
-
 };
 
+document
+.querySelector(".search button")
+.addEventListener("click", function(){
+weather.search();
+});
 
-// for current location
-window.addEventListener("load" ,()=>{
+document
+.querySelector(".search-bar")
+.addEventListener("keyup", function(event){
+    if(event.key=="Enter"){
+        weather.search();
+    }
+});
 
-let long;
-let lat;
-
-if(navigator.geolocation)
-{
-
-    navigator.geolocation.getCurrentPosition((position)=>
-    {
-
-   
-    
-    long=position.coords.longitude;
-    lat=position.coords.latitude;
-
-        const api=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=dab3af44de7d24ae7ff86549334e45bd     `
-
-        fetch(api).then((response)=>{
-
-            return response.json();
-
-
-        })
-
-        .then (data =>
-            {
-
-                    const{name}=data;
-                    const{feels_like}=data.main;
-                    const{id,main}=data.weather[0];
-
-
-                    loc.textContent=name;
-                    climate.textContent=main;
-                    tempvalue.textContent=(feels_like-273).toFixed(2);
-                    if(id<300 && id>200)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/thunderstorm.svg"
-                    }
-                   else  if(id<400 && id>300)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/cloud-solid.svg"
-                    }
-                   else if(id<600&& id>500)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/rain.svg"
-                    }
-                   else  if(id<700 && id>600)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/snow.svg"
-                    }
-                   else  if(id<800 && id>700)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/clouds.svg"
-                    }
-                     else if(id==800)
-                    {
-                        tempicon.src="../assets/Images/weather_icons/clouds-and-sun.svg"
-                    }
-
-                    console.log(data);
-
-                    //Change Temperature to Celsius/Fahrenheit
-                    temperatureSection.addEventListener('click' , ()=>{
-                        if(tempunit.textContent==="F"){
-                            tempunit.textContent = "°C";
-                            tempvalue.textContent=(feels_like-273).toFixed(2);
-                            
-                        }
-                        else{
-                            tempunit.textContent = "F";
-                            tempvalue.textContent= 
-                            ((feels_like- 273.15) * 9/5+ 32).toFixed(2);
-                        }
-                    });
-            });
-}
-
-    )}
-
-})
-
+weather.fetchWeather("Jhansi");
