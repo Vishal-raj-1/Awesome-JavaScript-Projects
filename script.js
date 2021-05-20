bd13debd5c90a6646bb23daea749e992ad880862
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2021-05-21 01:09:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ let projectData = [
     projectUrl: "public/binary_circle.html"
   },
   {
+    projectName: "Circus Game",
+    projectImage: "assets/GIFs/circus.png",
+    projectUrl: "public/circus.html",
+  },
+  {
     projectName:"Emoji Feedback Form",
     projectImage: "assets/GIFs/feedback.gif",
     projectUrl: "public/feedback.html",
