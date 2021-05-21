31ff22d24cb8ff4374ea1af65816f50932137420
Shivangi-ch <57783826+Shivangi-ch@users.noreply.github.com>
2021-05-21 11:50:56 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ let projectData = [
     projectUrl: "public/PigGame.html",
   },
   {
+    projectName: "Smart Temperature Slider",
+    projectImage: "assets/GIFs/temp_slider.gif",
+    projectUrl: "public/temp_slider.html",
+  },
+  {
     projectName: "Work Time Tracker",
     projectImage: "assets/GIFs/timetracker.gif",
     projectUrl: "public/timetracker.html",
