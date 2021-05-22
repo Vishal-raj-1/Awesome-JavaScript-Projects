825370ee6db80771f9ec185887025f5507565bae
ABHISHEK KUMAR <56838919+Abhikr-git@users.noreply.github.com>
2021-05-22 23:55:30 +0530

    Added Photo Details teller project

--- script.js
+++ script.js
@@ -908,6 +908,11 @@ let projectData = [
     projectUrl: "public/wordScramble.html"
   },
   {
+    projectName: "Photo Details Teller",
+    projectImage: "assets/Images/photoDetails.png",
+    projectUrl: "public/photoDetails.html"
+  },
+  {
     projectName: "Guess the song",
     projectImage: "assets/Images/guess_song/view.png",
     projectUrl: "public/guess_song.html"
