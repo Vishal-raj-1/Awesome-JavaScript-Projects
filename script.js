4167f1c4bc53c69145efd842ff4ca78afad78458
Vanshika <vanshikadeepakchokhani@gmail.com>
2021-05-23 10:55:47 +0530

    Form Validator Project updated

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ let projectData = [
   },
   {
     projectName: "FORM VALIDATE",
-    projectImage: "assets/Images/formvalidate.png",
+    projectImage: "assets/Images/formValidate.png",
     projectUrl: "public/formvalidate.html"
   },
   {
