b3c673395a1f7218614ba8a08739454ba077bb17
Tidbits JS <tidbitsjs@gmail.com>
2021-05-25 19:08:05 +0530

    traffic lights

--- script.js
+++ script.js
@@ -477,6 +477,11 @@ let projectData = [
     projectUrl: "public/imagePuzzleGame.html"
   },
   {
+    projectName: "Traffic Lights",
+    projectImage: "assets/GIFs/trafficLights.gif",
+    projectUrl: "public/trafficLights.html"
+  },
+  {
     projectName: "Bubble Sort",
     projectImage: "assets/GIFs/bubbleSort.gif",
     projectUrl: "public/bubbleSort.html"
