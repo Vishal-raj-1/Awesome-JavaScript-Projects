0e66c7cb9361e1a7e2a2e5dadc0af71029aa1617
Amit Kumar Mishra <60662775+Amit366@users.noreply.github.com>
2021-05-25 11:31:06 +0530

    Update sketch.js

--- assets/js/Minesweep_js/sketch.js
+++ assets/js/Minesweep_js/sketch.js
@@ -1,9 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Minesweeper
-// Video: https://youtu.be/LFU5ZlrR21E
 
 function make2DArray(cols, rows) {
   var arr = new Array(cols);
