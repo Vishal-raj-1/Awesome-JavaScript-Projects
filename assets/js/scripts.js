075de1c984dd1e85f03f264c8dc01b2a880dc5d6
Amrutha1101 <63070698+Amrutha1101@users.noreply.github.com>
2021-07-04 23:17:26 +0530

    linkActive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,6 +25,33 @@ $(document).ready(function()
     
 });
 
+
+
+
+let menuBar = document.getElementById('menuBar');
+let menus = document.getElementById('menu');
+let menuCloses = document.getElementById('menuClose');
+
+function menu (){
+    menus.style.right= '0';
+    menuCloses.style.display= 'inherit';
+}
+function menuClose(){
+    menus.style.right="-280px";
+    menuCloses.style.display= 'none';
+}
+
+window.onscroll = function()
+{
+    if(pageYOffset >= 90)
+    {
+        document.getElementById('goToTop').style.visibility="visible";
+    }else
+    {
+        document.getElementById('goToTop').style.visibility="hidden";
+    }
+};
+
 navbar = document.querySelector(".navbar").querySelectorAll("a");
 console.log(navbar);
 
@@ -35,5 +62,3 @@ navbar.forEach(element =>{
         this.classList.add("active");
     })
 } );
-
-
